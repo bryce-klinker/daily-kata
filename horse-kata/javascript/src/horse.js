2f0a45c9c5f51629398396fd1b2a10d4d0924a18
Bryce Klinker <bklinker@pillartechnology.com>
2019-12-09 09:18:26 -0600

    Removing old filter method that is no longer used

--- horse-kata/javascript/src/horse.js
+++ horse-kata/javascript/src/horse.js
@@ -12,11 +12,3 @@ export function filterSortPaginateTable(headers, tableData, filters, sortMetadat
   // TODO: paginate horse table using paginationMetadata
   return new PaginatedTable(headers, dataTable.tableData);
 }
-
-function filterTableData(filters, headers, tableData) {
-  filters.forEach(filter => {
-    const filterIndex = headers.indexOf(filter.column);
-    tableData = tableData.filter(i => i[filterIndex] === filter.value);
-  });
-  return tableData;
-}
