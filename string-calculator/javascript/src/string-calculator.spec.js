31c41f426c1b87463119f1f9f606e1ce8099390d
Bryce Klinker <bklinker@pillartechnology.com>
2019-12-10 12:19:19 -0600

    Refactoring tests to be quicker to write

--- string-calculator/javascript/src/string-calculator.spec.js
+++ string-calculator/javascript/src/string-calculator.spec.js
@@ -4,27 +4,27 @@ import { calculateSum } from './string-calculator';
 
 describe('String Calculator', () => {
   it('should return zero for empty string', () => {
-    const sum = calculateSum('');
-    expect(sum).to.eql(0);
+    assertCalculateSum('', 0);
   });
 
   it('should return 4 for string \'4\'', () => {
-    const sum = calculateSum('4');
-    expect(sum).to.eql(4);
+    assertCalculateSum('4', 4);
   });
 
   it('should return 13 for string \'13\'', () => {
-    const sum = calculateSum('13');
-    expect(sum).to.eql(13);
+    assertCalculateSum('13', 13);
   });
 
   it('should return 7 for string \'4,3\'', () => {
-    const sum = calculateSum('4,3');
-    expect(sum).to.eql(7);
+    assertCalculateSum('4,3', 7);
   });
 
   it('should return 13 for string \'6,7\'', () => {
-    const sum = calculateSum('6,7');
-    expect(sum).to.eql(13);
+    assertCalculateSum('6,7', 13);
   });
+
+  function assertCalculateSum(input, expected) {
+    const sum = calculateSum(input);
+    expect(sum).to.eql(expected);
+  }
 });
