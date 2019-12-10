c42ef41650d869534dddb865c3b2719d9d713722
Bryce Klinker <bklinker@pillartechnology.com>
2019-12-10 12:10:05 -0600

    First requirement is done

---
+++ string-calculator/javascript/src/string-calculator.js
@@ -0,0 +1,3 @@
+export function calculateSum() {
+  return 0;
+}
--- string-calculator/javascript/src/string-calculator.spec.js
+++ string-calculator/javascript/src/string-calculator.spec.js
@@ -1,5 +1,10 @@
 import { describe } from 'mocha';
+import { expect } from 'chai';
+import { calculateSum } from './string-calculator';
 
 describe('String Calculator', () => {
-
+  it('should return zero for empty string', () => {
+    const sum = calculateSum('');
+    expect(sum).to.eql(0);
+  });
 });
