764ff35739a22fdede1d84e5e033ab52ff054587
Bryce Klinker <bklinker@pillartechnology.com>
2019-12-11 08:47:24 -0600

    Moving Sulfuras class to separate file (switch smell)

--- gilded-rose/c-sharp/src/GildedRose/GildedRose.cs
+++ gilded-rose/c-sharp/src/GildedRose/GildedRose.cs
@@ -36,12 +36,4 @@ namespace GildedRose
             }
         }
     }
-
-    internal class Sulfuras : Item
-    {
-        public Sulfuras(Item item)
-            : base(item.Name, item.SellIn, item.Quality)
-        {
-        }
-    }
 }
---
+++ gilded-rose/c-sharp/src/GildedRose/Sulfuras.cs
@@ -0,0 +1,10 @@
+namespace GildedRose
+{
+    internal class Sulfuras : Item
+    {
+        public Sulfuras(Item item)
+            : base(item.Name, item.SellIn, item.Quality)
+        {
+        }
+    }
+}
