325a3bce6f884c8c4cc08af86277c364d91ed9b2
Bryce Klinker <bklinker@pillartechnology.com>
2019-12-11 08:24:41 -0600

    Moving AgedBrie to separate file (C# conventions)

---
+++ gilded-rose/c-sharp/src/GildedRose/AgedBrie.cs
@@ -0,0 +1,11 @@
+namespace GildedRose
+{
+    internal class AgedBrie : Item
+    {
+        public AgedBrie(Item item)
+            : base(item.Name, item.SellIn, item.Quality)
+        {
+            
+        }
+    }
+}
--- gilded-rose/c-sharp/src/GildedRose/GildedRose.cs
+++ gilded-rose/c-sharp/src/GildedRose/GildedRose.cs
@@ -32,13 +32,4 @@ namespace GildedRose
             }
         }
     }
-
-    internal class AgedBrie : Item
-    {
-        public AgedBrie(Item item)
-            : base(item.Name, item.SellIn, item.Quality)
-        {
-            
-        }
-    }
 }
