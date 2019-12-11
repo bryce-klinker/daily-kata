7177b938ab412166ee22dce3732a488a0cfd9013
Bryce Klinker <bklinker@pillartechnology.com>
2019-12-11 08:47:02 -0600

    Added Sulfuras class (switch smell)

--- gilded-rose/c-sharp/src/GildedRose/GildedRose.cs
+++ gilded-rose/c-sharp/src/GildedRose/GildedRose.cs
@@ -36,4 +36,12 @@ namespace GildedRose
             }
         }
     }
+
+    internal class Sulfuras : Item
+    {
+        public Sulfuras(Item item)
+            : base(item.Name, item.SellIn, item.Quality)
+        {
+        }
+    }
 }
