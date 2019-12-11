c665c5554bf803358d9322205397a214f92fa5d4
Bryce Klinker <bklinker@pillartechnology.com>
2019-12-11 08:36:03 -0600

    Adding backstage pass derived from Item (switch smell)

--- gilded-rose/c-sharp/src/GildedRose/GildedRose.cs
+++ gilded-rose/c-sharp/src/GildedRose/GildedRose.cs
@@ -34,4 +34,13 @@ namespace GildedRose
             }
         }
     }
+
+    internal class BackstagePass : Item
+    {
+        public BackstagePass(Item item)
+            : base(item.Name, item.SellIn, item.Quality)
+        {
+            
+        }
+    }
 }
