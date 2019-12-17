e42bc5a277efc9a23f0f002214b0477f8405fa65
Bryce Klinker <bklinker@pillartechnology.com>
2019-12-17 08:25:24 -0600

    Updating to pass first teset

--- card-game-war/python/tests/test_war_game.py
+++ card-game-war/python/tests/test_war_game.py
@@ -1,4 +1,18 @@
+class WarGame:
+    @property
+    def players(self):
+        return [1,1]
+
+    def start(self):
+        pass
+
+
 class TestWarGame:
 
-    def test_when_game_started_then_each_player_has_half_a_deck_of_cards(self):
-        pass
+    def test_when_game_started_then_game_should_have_two_players(self):
+        game = WarGame()
+
+        game.start()
+
+        assert len(game.players) == 2
+
