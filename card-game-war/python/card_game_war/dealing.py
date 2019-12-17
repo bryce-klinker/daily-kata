8610cbd899559e39c2db11eb0cc0583f087e463c
Bryce Klinker <bklinker@pillartechnology.com>
2019-12-17 10:41:05 -0600

    Starting to add the concept of cards

--- card-game-war/python/card_game_war/dealing.py
+++ card-game-war/python/card_game_war/dealing.py
@@ -1,5 +1,14 @@
 def deal_war_hands():
     return [
-        [],
-        []
+        [Card(2)] * 26,
+        [Card(2)] * 26
     ]
+
+
+class Card:
+    @property
+    def value(self):
+        return self.__value
+
+    def __init__(self, value):
+        self.__value = value
--- card-game-war/python/tests/test_dealing.py
+++ card-game-war/python/tests/test_dealing.py
@@ -3,7 +3,19 @@ from card_game_war.dealing import deal_war_hands
 
 class TestDealing:
 
-    def test_when_deck_is_dealt_then_returns_half_the_deck_to_each_hand(self):
+    def test_when_deck_is_dealt_then_returns_two_hands(self):
         hands = deal_war_hands()
 
         assert len(hands) == 2
+
+    def test_when_deck_is_dealt_then_returns_half_the_deck_to_each_hand(self):
+        hands = deal_war_hands()
+
+        assert len(hands[0]) == 26
+        assert len(hands[1]) == 26
+
+    def test_when_deck_is_dealt_then_two_hands_of_cards_are_dealt(self):
+        hands = deal_war_hands()
+
+        card = hands[0][0]
+        assert 1 < card.value < 15
