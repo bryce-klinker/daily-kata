18e3bdf7c2b97e132bf0bad622b840028d6745f4
Bryce Klinker <bklinker@pillartechnology.com>
2019-12-19 09:34:12 -0600

    Refactoring a bit and trying to figure out some more make stuff

--- fizzbuzz/c++/src/fizzbuzz.cpp
+++ fizzbuzz/c++/src/fizzbuzz.cpp
@@ -7,9 +7,23 @@ bool is_divisible_by(int divisor, int number)
     return number % divisor == 0;
 }
 
+bool is_divisible_by(int *divisors, int number, int array_size)
+{
+    for (int i = 0; i < array_size; ++i) {
+        if (!is_divisible_by(divisors[i], number))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+
 string fizzbuzz(int number)
 {
-    if (is_divisible_by(15, number))
+    int three_and_five[] = {3, 5};
+    size_t array_size = sizeof(three_and_five) / sizeof(int);
+    if (is_divisible_by(three_and_five, number, array_size))
     {
         return "fizzbuzz";
     }
