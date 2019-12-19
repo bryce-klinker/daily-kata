0678891eb979fc67ae8ad3fce47063b6193d5c9e
Bryce Klinker <bklinker@pillartechnology.com>
2019-12-18 23:18:06 -0600

    Moving readers to readers namespace

--- evercraft-code-smells/c-sharp/src/Smelly.Code.Core/EvercraftGame.cs
+++ evercraft-code-smells/c-sharp/src/Smelly.Code.Core/EvercraftGame.cs
@@ -1,9 +1,7 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using Microsoft.Data.Sqlite;
-using Newtonsoft.Json.Linq;
+using Smelly.Code.Core.Readers;
 
 namespace Smelly.Code.Core
 {
@@ -328,64 +326,6 @@ namespace Smelly.Code.Core
                     return new JsonCharacterReader();
                 default:
                     return new CharacterReader();
-            }
-        }
-    }
-
-    internal class JsonCharacterReader : CharacterReader
-    {
-        public override IEnumerable<Character> ReadCharacters(string filePath)
-        {
-            var jObject = JObject.Parse(File.ReadAllText(filePath));
-            var characters = jObject.Value<JArray>("characters");
-            for (var i = 0; i < characters.Count; i++)
-            {
-                yield return new Character(characters[i].Value<string>("name"), 5, characters[i].Value<int>("arm"),
-                    characters[i].Value<int>("str"), characters[i].Value<int>("dex"),
-                    characters[i].Value<int>("const"));
-            }
-        }
-    }
-
-    public class CharacterReader
-    {
-        public virtual IEnumerable<Character> ReadCharacters(string filePath)
-        {
-            if (filePath.EndsWith(".db"))
-            {
-                using (var connection = new SqliteConnection($"Data Source={filePath}"))
-                {
-                    connection.Open();
-
-                    var selectCharactersText = "select [Name], Armor, Str, Dex, Const from [Characters]";
-                    using (var command = new SqliteCommand(selectCharactersText, connection))
-                    {
-                        using (var reader = command.ExecuteReader())
-                        {
-                            var current = 0;
-                            while (reader.Read())
-                            {
-                                yield return new Character(reader.GetFieldValue<string>(0), 5,
-                                    reader.GetFieldValue<int>(1), reader.GetFieldValue<int>(2),
-                                    reader.GetFieldValue<int>(3),
-                                    reader.GetFieldValue<int>(4));
-                                current = current + 1;
-                            }
-                        }
-                    }
-                }
-            }
-            else
-            {
-                var lines = File.ReadAllLines(filePath)
-                    .Skip(1)
-                    .ToArray();
-                for (var i = 0; i < lines.Length; i++)
-                {
-                    var vs = lines[i].Split(',');
-                    yield return new Character(vs[0], 5, int.Parse(vs[1]), int.Parse(vs[2]), int.Parse(vs[3]),
-                        int.Parse(vs[4]));
-                }
             }
         }
     }
---
+++ evercraft-code-smells/c-sharp/src/Smelly.Code.Core/Readers/JsonCharacterReader.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace Smelly.Code.Core.Readers
+{
+    internal class JsonCharacterReader : CharacterReader
+    {
+        public override IEnumerable<Character> ReadCharacters(string filePath)
+        {
+            var jObject = JObject.Parse(File.ReadAllText(filePath));
+            var characters = jObject.Value<JArray>("characters");
+            for (var i = 0; i < characters.Count; i++)
+            {
+                yield return new Character(characters[i].Value<string>("name"), 5, characters[i].Value<int>("arm"),
+                    characters[i].Value<int>("str"), characters[i].Value<int>("dex"),
+                    characters[i].Value<int>("const"));
+            }
+        }
+    }
+}
