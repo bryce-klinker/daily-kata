061e6a15c6ac281847f9f11171cedd7fc246191a
Bryce Klinker <bklinker@pillartechnology.com>
2019-12-18 23:47:51 -0600

    USing character attribute in character class

--- evercraft-code-smells/c-sharp/src/Smelly.Code.Core/Character.cs
+++ evercraft-code-smells/c-sharp/src/Smelly.Code.Core/Character.cs
@@ -1,22 +1,43 @@
+using System.Collections.Generic;
+
 namespace Smelly.Code.Core
 {
     public class Character
     {
+        private readonly Dictionary<string, CharacterAttribute> _attributes;
         public string Name { get; set; }
         public int HitPts { get; set; }
         public int Arm { get; set; }
-        public int? Str { get; set; }
-        public int? Dex { get; set; }
-        public int? Const { get; set; }
+
+        public int? Str
+        {
+            get => _attributes[nameof(Str)];
+            set => _attributes[nameof(Str)] = value;
+        }
+
+        public int? Dex
+        {
+            get => _attributes[nameof(Dex)];
+            set => _attributes[nameof(Dex)] = value;
+        }
+
+        public int? Const
+        {
+            get => _attributes[nameof(Const)];
+            set => _attributes[nameof(Const)] = value;
+        }
 
         public Character(string name, int hitPts, int arm, int? str, int? dex, int? @const)
         {
             HitPts = hitPts;
             Arm = arm;
-            Str = str;
-            Dex = dex;
-            Const = @const;
             Name = name;
+            _attributes = new Dictionary<string, CharacterAttribute>
+            {
+                {nameof(Str), new CharacterAttribute(str)},
+                {nameof(Dex), new CharacterAttribute(dex)},
+                {nameof(Const), new CharacterAttribute(@const)},
+            };
         }
 
         public void EquipArmor(ArmorType armorType, int weight)
