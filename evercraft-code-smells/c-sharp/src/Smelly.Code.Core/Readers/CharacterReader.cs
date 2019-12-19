9b115297c2d98c3ceceeb414755f4259599bf30e
Bryce Klinker <bklinker@pillartechnology.com>
2019-12-18 23:22:31 -0600

    Removing unneeded code from character reader

--- evercraft-code-smells/c-sharp/src/Smelly.Code.Core/Readers/CharacterReader.cs
+++ evercraft-code-smells/c-sharp/src/Smelly.Code.Core/Readers/CharacterReader.cs
@@ -9,41 +9,14 @@ namespace Smelly.Code.Core.Readers
     {
         public virtual IEnumerable<Character> ReadCharacters(string filePath)
         {
-            if (filePath.EndsWith(".db"))
+            var lines = File.ReadAllLines(filePath)
+                .Skip(1)
+                .ToArray();
+            for (var i = 0; i < lines.Length; i++)
             {
-                using (var connection = new SqliteConnection($"Data Source={filePath}"))
-                {
-                    connection.Open();
-
-                    var selectCharactersText = "select [Name], Armor, Str, Dex, Const from [Characters]";
-                    using (var command = new SqliteCommand(selectCharactersText, connection))
-                    {
-                        using (var reader = command.ExecuteReader())
-                        {
-                            var current = 0;
-                            while (reader.Read())
-                            {
-                                yield return new Character(reader.GetFieldValue<string>(0), 5,
-                                    reader.GetFieldValue<int>(1), reader.GetFieldValue<int>(2),
-                                    reader.GetFieldValue<int>(3),
-                                    reader.GetFieldValue<int>(4));
-                                current = current + 1;
-                            }
-                        }
-                    }
-                }
-            }
-            else
-            {
-                var lines = File.ReadAllLines(filePath)
-                    .Skip(1)
-                    .ToArray();
-                for (var i = 0; i < lines.Length; i++)
-                {
-                    var vs = lines[i].Split(',');
-                    yield return new Character(vs[0], 5, int.Parse(vs[1]), int.Parse(vs[2]), int.Parse(vs[3]),
-                        int.Parse(vs[4]));
-                }
+                var vs = lines[i].Split(',');
+                yield return new Character(vs[0], 5, int.Parse(vs[1]), int.Parse(vs[2]), int.Parse(vs[3]),
+                    int.Parse(vs[4]));
             }
         }
     }
--- evercraft-code-smells/c-sharp/src/Smelly.Code.Core/Readers/SqliteCharacterReader.cs
+++ evercraft-code-smells/c-sharp/src/Smelly.Code.Core/Readers/SqliteCharacterReader.cs
@@ -1,6 +1,4 @@
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using Microsoft.Data.Sqlite;
 
 namespace Smelly.Code.Core.Readers
@@ -9,27 +7,27 @@ namespace Smelly.Code.Core.Readers
     {
         public override IEnumerable<Character> ReadCharacters(string filePath)
         {
-                using (var connection = new SqliteConnection($"Data Source={filePath}"))
-                {
-                    connection.Open();
+            using (var connection = new SqliteConnection($"Data Source={filePath}"))
+            {
+                connection.Open();
 
-                    var selectCharactersText = "select [Name], Armor, Str, Dex, Const from [Characters]";
-                    using (var command = new SqliteCommand(selectCharactersText, connection))
+                var selectCharactersText = "select [Name], Armor, Str, Dex, Const from [Characters]";
+                using (var command = new SqliteCommand(selectCharactersText, connection))
+                {
+                    using (var reader = command.ExecuteReader())
                     {
-                        using (var reader = command.ExecuteReader())
+                        var current = 0;
+                        while (reader.Read())
                         {
-                            var current = 0;
-                            while (reader.Read())
-                            {
-                                yield return new Character(reader.GetFieldValue<string>(0), 5,
-                                    reader.GetFieldValue<int>(1), reader.GetFieldValue<int>(2),
-                                    reader.GetFieldValue<int>(3),
-                                    reader.GetFieldValue<int>(4));
-                                current = current + 1;
-                            }
+                            yield return new Character(reader.GetFieldValue<string>(0), 5,
+                                reader.GetFieldValue<int>(1), reader.GetFieldValue<int>(2),
+                                reader.GetFieldValue<int>(3),
+                                reader.GetFieldValue<int>(4));
+                            current = current + 1;
                         }
                     }
                 }
+            }
         }
     }
 }
